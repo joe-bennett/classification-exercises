482c4e369bcc5d0bf626c1ee0ff4d142f3ce6c0d
joe_bennett <joe.d.bennett01@gmail.com>
2022-08-16 16:57:02 -0500

    updated files

--- prepare.py
+++ prepare.py
@@ -1,41 +1,69 @@
+#standard ds libraries
 import pandas as pd
 import numpy as np
+
+# import splitting functions
 from sklearn.model_selection import train_test_split
-import seaborn as sns
 
 
-def prep_iris(iris_db):
-    iris_db.drop(['species_id', 'measurement_id'], axis=1, inplace=True)
-    iris_db.rename(columns={'species_name':'species'},inplace=True)
-    dummy_df=pd.get_dummies(iris_db.species)
-    iris_db=pd.concat([iris_db,dummy_df], axis=1)
-    return iris_db
+def prep_iris(iris):
+    iris = iris.drop(columns=['species_id','measurement_id'])
+    iris = iris.rename(columns={'species_name':'species'})
+    dummy_iris = pd.get_dummies(iris.species, drop_first=True)
+    iris = pd.concat([iris, dummy_iris], axis=1)
+    return iris
 
-def prep_titanic(titanic_db):
-    titanic_db.drop(['passenger_id','class','embark_town'],axis=1, inplace=True)
-    dum_var=pd.get_dummies(titanic_db[['sex','embarked']],drop_first=True)
-    titanic_db=pd.concat([titanic_db,dum_var],axis=1)
-    return titanic_db
+
+def prep_titanic(titanic):
+    titanic = titanic.drop(columns=['embarked','class', 'age','deck'])
+    dummy_df = pd.get_dummies(data=titanic[['sex','embark_town']], drop_first=True)
+    titanic = pd.concat([titanic, dummy_df], axis=1)
+    titanic=titanic.drop(columns=['embark_town','sex'])
     
+    return titanic
 
 
-def prep_telco(telco_db):
-    telco_db.drop(['payment_type_id','internet_service_type_id','contract_type_id'], axis=1, inplace=True)
-    dum_var=pd.get_dummies(telco_db[['gender','partner','dependents','phone_service','multiple_lines','online_security',
-                                'device_protection','tech_support','streaming_tv','streaming_movies','paperless_billing','churn', 'internet_service_type','payment_type']],drop_first=True)
-    telco_db=pd.concat([telco_db,dum_var],axis=1)
-    return telco_db
+def prep_telco(telco):
+    telco = telco.drop(columns=['internet_service_type_id', 'contract_type_id', 'payment_type_id'])
+
+    telco['gender_encoded'] = telco.gender.map({'Female': 1, 'Male': 0})
+    telco['partner_encoded'] = telco.partner.map({'Yes': 1, 'No': 0})
+    telco['dependents_encoded'] = telco.dependents.map({'Yes': 1, 'No': 0})
+    telco['phone_service_encoded'] = telco.phone_service.map({'Yes': 1, 'No': 0})
+    telco['paperless_billing_encoded'] = telco.paperless_billing.map({'Yes': 1, 'No': 0})
+    telco['churn_encoded'] = telco.churn.map({'Yes': 1, 'No': 0})
+    
+    dummy_df = pd.get_dummies(telco[['multiple_lines', \
+                              'online_security', \
+                              'online_backup', \
+                              'device_protection', \
+                              'tech_support', \
+                              'streaming_tv', \
+                              'streaming_movies', \
+                              'contract_type', \
+                              'internet_service_type', \
+                              'payment_type'
+                            ]],
+                              drop_first=True)
+    telco = pd.concat( [telco, dummy_df], axis=1 )
+    
+    return telco
 
 
-
-
-def split_data(df, target):
-    '''
-    Takes in a dataframe and return train, validate, test subset dataframes
-    '''
-    train, test = train_test_split(df, test_size=.2, 
-                               random_state=123, stratify=df[target])
-    train, validate = train_test_split(train, test_size=.25, 
-                 random_state=123, stratify=train[target])
+def my_train_test_split(df, target):
+    
+    train, test = train_test_split(df, test_size=.2, random_state=123, stratify=df[target])
+    train, validate = train_test_split(train, test_size=.25, random_state=123, stratify=train[target])
     
     return train, validate, test
+
+
+
+X_train = train.drop(columns=['species'])
+y_train = train.species
+
+X_validate = validate.drop(columns=['species'])
+y_validate = validate.species
+
+X_test = test.drop(columns=['species'])
+y_test = test.species
